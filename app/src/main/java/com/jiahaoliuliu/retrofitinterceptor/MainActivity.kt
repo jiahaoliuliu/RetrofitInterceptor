1b0c694e4e4d481841fd139380743e94b8ab72b0
Jiahao Liu <jiahao.liu@dnata.com>
2019-09-24 11:21:37 +0400

    Created a simple interface to make a backend request

--- app/src/main/java/com/jiahaoliuliu/retrofitinterceptor/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/retrofitinterceptor/MainActivity.kt
@@ -2,11 +2,22 @@ package com.jiahaoliuliu.retrofitinterceptor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.jiahaoliuliu.retrofitinterceptor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+//    lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+        binding.makeAWishButton.setOnClickListener{makeAWish()}
+    }
+
+    private fun makeAWish() {
+        Toast.makeText(this, "Making your wish come to true", Toast.LENGTH_LONG).show()
     }
 }
